05a3a051f00630f346096c72291fe8c0486324a0
SergStr <sergstrateg@yandex.ru>
2022-04-16 20:18:27 +0300

    Firebase

--- app/src/main/java/com/example/laba3/MainActivity.kt
+++ app/src/main/java/com/example/laba3/MainActivity.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.laba3.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import java.security.MessageDigest
 
 
@@ -15,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var passwordTextView: TextView
     lateinit var submitButton: Button
     lateinit var authError:TextView
+    private lateinit var database: FirebaseFirestore
     private var userList: Map<String, String> = mapOf(
         "test@test.com" to "65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5", // qwerty
         "admin@test.com" to "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92" // 123456
@@ -32,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         submitButton = findViewById(R.id.submitButton)
         emailTextView.text = "Avadakedavra"
         authError = findViewById(R.id.authError)
+        setUpDatabase()
         /*submitButton.setOnClickListener {
             val intent = Intent(this,RecyclerActivity::class.java )
             startActivity(intent)
@@ -62,6 +68,16 @@ class MainActivity : AppCompatActivity() {
             .fold("") { str, it -> str + "%02x".format(it) }
     }
 
+    private fun setUpDatabase(){
+        database = FirebaseFirestore.getInstance()
+        val data = hashMapOf(
+            "user_id" to 1,
+            "name" to "test@test.com",
+            "password" to "65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5",
+            "role" to "pleb"
+        )
+        database.collection("database").document("users").set(data)
+    }
     private fun authorize(email: String, password: String): Boolean
     {
         if (this.userList.contains(email)) {
