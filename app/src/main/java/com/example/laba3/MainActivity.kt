5654a2b88551cbd16875c7b5166a045b9a64a673
SergStr <sergstrateg@yandex.ru>
2022-04-23 15:37:42 +0300

    test 2.00000001

--- app/src/main/java/com/example/laba3/MainActivity.kt
+++ app/src/main/java/com/example/laba3/MainActivity.kt
@@ -65,7 +65,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    public fun hashString(input: String): String {
+     private fun hashString(input: String): String {
         return MessageDigest.getInstance("SHA-256")
             .digest(input.toByteArray())
             .fold("") { str, it -> str + "%02x".format(it) }
--- app/src/test/java/com/example/laba3/ExampleUnitTest.kt
+++ app/src/test/java/com/example/laba3/ExampleUnitTest.kt
@@ -15,9 +15,4 @@ class ExampleUnitTest {
         assertEquals(4, 2 + 2)
     }
 
-    @Test
-    fun hash_func() {
-        val mainActivity = MainActivity()
-        assertEquals("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", mainActivity.hashString("123456"))
-    }
 }
